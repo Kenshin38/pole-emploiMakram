f3b431592d5d929296d6478d43b65cd43f5f92e8
Kenshin38 <makramourguedi@gmail.com>
2020-03-26 09:38:19 +0100

    Création entité Job/Employee

---
+++ src/Entity/Employee.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\EmployeeRepository")
+ */
+class Employee
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Firstname;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Lastname;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $employement_date;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFirstname(): ?string
+    {
+        return $this->Firstname;
+    }
+
+    public function setFirstname(string $Firstname): self
+    {
+        $this->Firstname = $Firstname;
+
+        return $this;
+    }
+
+    public function getLastname(): ?string
+    {
+        return $this->Lastname;
+    }
+
+    public function setLastname(string $Lastname): self
+    {
+        $this->Lastname = $Lastname;
+
+        return $this;
+    }
+
+    public function getEmployementDate(): ?\DateTimeInterface
+    {
+        return $this->employement_date;
+    }
+
+    public function setEmployementDate(\DateTimeInterface $employement_date): self
+    {
+        $this->employement_date = $employement_date;
+
+        return $this;
+    }
+}
